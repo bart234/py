0661b8f1e3916125420a095c2af7edb3538a10bd
bart234 <bartlomiej.p.biernacki@o2.pl>
2024-11-30 17:28:45 +0100

    0001_1generator in progress

--- 0001_projects/1_report_generator_stock_consumption/cfg_app_data_generation/main.py
+++ 0001_projects/1_report_generator_stock_consumption/cfg_app_data_generation/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from pathlib import Path
 import os
 import yaml
@@ -21,8 +22,37 @@ def load_file()->dict:
             print(exc)
     return dict_for_config
 
-def process_config(dict_for_config):
-    types_in_dict = dict_for_config['cfg_setup']
+
+
+class ProcessConfig:
+    def __init__(self,dict_for_config) -> None:
+        self.types_in_dict = dict_for_config['cfg_setup']
+        self.types_in_dict = dict_for_config['products']
+        self.wh_stock = None
+        self.consumption = None
+
+    class Product:
+        def __init__(self, name, wh_stock,consumption) -> None:
+            self.name =name
+            self.wh_stock = wh_stock
+            self.consumption=consumption
+        #TODO: finish class and do preparing  functions
+    def prepare_wh_stock_and_consumption():
+        wh_stock =pd.DataFrame({self._consumption_data_cols[0]:[2,22,222],
+                                                   self._consumption_data_cols[1]:[5,50,500]},index=self._product_list)
+
+
+self._wh_stock_consumption = pd.DataFrame({self._consumption_data_cols[0]:[2,22,222],
+                                                   self._consumption_data_cols[1]:[5,50,500]},index=self._product_list)
+
+        
+self._order_data_cols=['orders','days_to_stock']    #columns from order data 
+self._client_data_ord_pork =pd.DataFrame({self._order_data_cols[0]:np.random.randint(low=1,high=8,size=self._size_of_generated_arr),
+                                            self._order_data_cols[1]:np.random.randint(low=2,high=8,size=self._size_of_generated_arr)})
+self._client_data_ord_beef=pd.DataFrame({self._order_data_cols[0]:np.random.randint(low=5,high=45,size=self._size_of_generated_arr),
+                                            self._order_data_cols[1]:np.random.randint(low=2,high=18,size=self._size_of_generated_arr)})
+self._client_data_ord_wheat =pd.DataFrame({self._order_data_cols[0]:np.random.randint(low=45,high=745,size=self._size_of_generated_arr),
+                                            self._order_data_cols[1]:np.random.randint(low=2,high=22,size=self._size_of_generated_arr)})
 
     
 
@@ -36,4 +66,4 @@ def check_if_file_exist():
 
 d =load_file()
 print(d)
-process_config(d)
+p = ProcessConfig(d)
